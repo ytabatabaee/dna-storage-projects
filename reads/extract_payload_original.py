5ed0d240cfe37ced07c238f7371c84f6e90d5702
y-tabatabaee76 <yasamin.tabatabaee@gmail.com>
2020-11-19 10:35:53 +0330

    optimized

--- reads/extract_payload_original.py
+++ reads/extract_payload_original.py
@@ -8,6 +8,8 @@ EDIT_DIST_THRESHOLD = 3
 READS_FILE = sys.argv[1]
 PRIMERS_FILE = sys.argv[2]
 STRAND_LEN = 110
+DIR_FILE = '5pto3p.txt'
+REV_FILE = '3pto5p.txt'
 
 def read_sequences(file_name):
     '''read sequences from fastq file'''
@@ -42,61 +44,75 @@ def reverse_complement(s):
     return t
 
 
-def approximate_match(p, t, threshold=EDIT_DIST_THRESHOLD):
+def approximate_match(pl, pr, t, threshold=EDIT_DIST_THRESHOLD):
     '''approximate matching with edit distance'''
-    pos = -1
-    min_dist = len(p)
-    for i in range(len(t) - len(p) + 1):
-        if editdistance.eval(t[i:i + len(p)], p) <= min_dist:
-            min_dist = editdistance.eval(t[i:i + len(p)], p)
+    p1, p2 = -1, -1
+    min_dist = len(pl)
+    for i in range(len(t) - len(pl) + 1):
+        if editdistance.eval(t[i:i + len(pl)], pl) <= min_dist:
+            min_dist = editdistance.eval(t[i:i + len(pl)], pl)
             if min_dist <= threshold:
-                pos = i  
-    return pos
+                p1 = i  
+            if min_dist == 0:
+                break
+    if p1 == -1:
+        return p1, p2
+    
+    min_dist = len(pr)
+    for i in reversed(range(len(t) - len(pr) + 1)):
+        if editdistance.eval(t[i:i + len(pr)], pr) <= min_dist:
+            min_dist = editdistance.eval(t[i:i + len(pr)], pr)
+            if min_dist <= threshold:
+                p2 = i  
+            if min_dist == 0:
+                break
+    return p1, p2
 
 
 def extract_payloads(primer1, primer2, reads_file_name):
     reads = read_sequences(reads_file_name)
-    f1 = open("5pto3p.txt", "a")
-    f2 = open("3pto5p.txt", "a")
+    direct_list = []
+    rev_list = []
+    primer1_rev = reverse_complement(primer1)
+    primer2_rev = reverse_complement(primer2)
     direct_count = 0
     rev_count = 0
     hist = [0] * 170
     for r in reads:
-        p1 = approximate_match(primer1, r)
-        p2 = approximate_match(primer2, r)
-        pr1 = approximate_match(reverse_complement(primer1), r)
-        pr2 = approximate_match(reverse_complement(primer2), r)
-        # print(p1, p2, pr1, pr2)
-        if p1 != -1 and p2 != -1:
+        p1, p2 = approximate_match(primer1, primer2, r)
+        if p2 != -1 and p2 != -1:
             payload = r[p1+len(primer1):p2] 
             if len(payload) >= 0.9 * STRAND_LEN and len(payload) <= 1.1 * STRAND_LEN:
-                f1.write(payload + '\n')
+                direct_list.append(payload)
                 direct_count += 1
                 hist[len(payload)] += 1
-        elif pr1 != -1 and pr2 != -1:
-            payload = r[pr2+len(primer2):pr1]  
-            if len(payload) >= 0.9 * STRAND_LEN and len(payload) <= 1.1 * STRAND_LEN:
-                f2.write(payload + '\n')
-                rev_count += 1
-                hist[len(payload)] += 1
         else:
-            # print('Failed\n')
-            continue
-    f1.close()
-    f2.close()
+            pr2, pr1 = approximate_match(primer2_rev, primer1_rev, r)
+            if pr1 != -1 and pr2 != -1:
+                payload = r[pr2+len(primer2):pr1]  
+                if len(payload) >= 0.9 * STRAND_LEN and len(payload) <= 1.1 * STRAND_LEN:
+                    rev_list.append(payload)
+                    rev_count += 1
+                    hist[len(payload)] += 1
+            else:
+                continue           
+    with open(DIR_FILE, "w") as f1:
+        f1.write("\n".join(direct_list))  
+    with open(REV_FILE, "w") as f2:
+        f2.write("\n".join(rev_list))  
     
     # just for test
     print('5->3 strands count', direct_count)
     print('3->5 strands count', rev_count)
     print('percentage of excluded strands:', (1 - (direct_count + rev_count) / len(reads))* 100)
     # print(hist)
-    plt.plot(range(int(0.9 * STRAND_LEN), int(1.1 * STRAND_LEN)), hist[int(0.9 * STRAND_LEN):int(1.1 * STRAND_LEN)])
-    plt.title('distribution of strands lengths')
-    plt.show()
+    #plt.plot(range(int(0.9 * STRAND_LEN), int(1.1 * STRAND_LEN)), hist[int(0.9 * STRAND_LEN):int(1.1 * STRAND_LEN)])
+    #plt.title('distribution of strands lengths')
+    #plt.show()
 
 if __name__ == "__main__": 
     primer1, primer2 = read_primers(PRIMERS_FILE)  
     start = timeit.default_timer()
     extract_payloads(primer1, primer2, READS_FILE)
     stop = timeit.default_timer()
-    print('Time (sec): ', stop - start) 
+    print('Time (sec): ', stop - start)
